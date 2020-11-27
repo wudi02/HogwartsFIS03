17c5cadd5818145ea8211ad2eb7d64d5608e12ca
born_2_loveu <1164051773@qq.com>
2020-11-27 18:13:10 +0800

    练习：发礼物

--- python_pratice/sendgifts/send.py
+++ python_pratice/sendgifts/send.py
@@ -1,7 +1,7 @@
 from gift import have_gift
 
 
-##发礼物的方法
+##发礼物的方法 把礼物的初始状态改变
 def send():
     print("发礼物啦")
     have_gift = True
--- python_pratice/sendgifts/show.py
+++ python_pratice/sendgifts/show.py
@@ -1,7 +1,7 @@
 from gift import have_gift
 
 
-##展示礼物的方法
+##展示礼物的方法 进行条件判断
 def show():
     if have_gift == True:
         print("收到礼物啦,么么哒")
